12e341e20d49de1d37ded8ea20e7fa8b87f405fa
irapanch <117355580+irapanch@users.noreply.github.com>
2025-04-17 11:52:06 +0300

    CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Разрешаем CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PATCH'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4300);
 }
